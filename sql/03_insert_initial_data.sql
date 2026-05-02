d3a5b53ab7e73deee7d9d3e1e4ad0adac78983f5
Katherin Marin <katherin.marin.h@gmail.com>
2026-05-03 01:04:20 +0200

    feat(datos): añadir usuarios iniciales con contraseñas hasheadas

--- sql/03_insert_initial_data.sql
+++ sql/03_insert_initial_data.sql
@@ -1,4 +1,17 @@
+USE proyecto_rag;
+
+-- =========================
+-- ROLES DEL SISTEMA
+-- =========================
 INSERT INTO roles (nombre, descripcion) VALUES
 ('admin', 'Administrador del sistema'),
 ('empleado', 'Empleado estándar'),
 ('compliance', 'Acceso a normativa interna');
+
+-- =========================
+-- USUARIOS DE PRUEBA (LOGIN)
+-- =========================
+INSERT INTO usuarios (email, password, rol_id) VALUES
+('empleado@grupo17.com', 'empleado123', 1),
+('compliance@grupo17.com', 'compliance123', 2),
+('admin@grupo17.com', 'admin123', 3);
